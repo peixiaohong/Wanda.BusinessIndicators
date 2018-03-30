0d459602faa745d230c5efb4fb1b549011ec3a6f
peixiaohong <peixiaohong1989@163.com>
2018-03-30 15:55:31 +0800

    log中被忽略的文件

---
+++ 1Framework/Lib.Core/Log/Formatters/LogFormatterFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Lib.Core;
+
+namespace Lib.Log
+{
+    internal class LogFormatterFactory
+    {
+        public static ILogFormatter GetFormatter(LogConfigurationElement formatterElement)
+        {
+            if (formatterElement != null)
+            {
+                try
+                {
+                    return (ILogFormatter)TypeCreator.CreateInstance(formatterElement.Type, formatterElement);
+                }
+                catch (Exception ex)
+                {
+                    throw new LogException("创建Formatter对象时出错：" + ex.Message, ex);
+                }
+            }
+            else
+                return null;
+        }
+    }
+}
