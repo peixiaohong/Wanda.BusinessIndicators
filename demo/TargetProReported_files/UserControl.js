6aae7e13f45c68ddcf61876382a595c0af2cb8d3
zhaotou94 <719007475@qq.com>
2018-04-11 10:24:00 +0800

    修改图标显示

--- demo/TargetProReported_files/UserControl.js
+++ demo/TargetProReported_files/UserControl.js
@@ -22,8 +22,7 @@ function setArrow_nLeft(sender) {
 
     switch (sender) {
         case "downLoadTemplate":
-            $(".tip_box").css("display", "block");
-            $(".tip").css("display", "block");
+
             $(".arrow_n").css("left", "80px");
             $("#PromptMessage").html("请下载填报模版。");
             $("#PromptMessage").removeClass("Tishi");
@@ -31,16 +30,14 @@ function setArrow_nLeft(sender) {
             return true;
             break;
         case "dataUpload":
-            $(".tip_box").css("display", "block");
-            $(".tip").css("display", "block");
+
             $(".arrow_n").css("left", "250px");
             $("#PromptMessage").html("请使用系统提供的填报模板导入本月数据，如已导入，再次导入将覆盖当前数据。");
             $("#PromptMessage").removeClass("Tishi");
             return true;
             break;
         case "missTargetReport":
-            $(".tip_box").css("display", "block");
-            $(".tip").css("display", "block");
+
             $(".arrow_n").css("left", "420px");
             $("#PromptMessage").html("请填补回情况，未完成原因及措施。");
             $("#PromptMessage").removeClass("Tishi");
@@ -48,8 +45,7 @@ function setArrow_nLeft(sender) {
             break;
 
         case "missCurrentTargetReport":
-            $(".tip_box").css("display", "block");
-            $(".tip").css("display", "block");
+
             $(".arrow_n").css("left", "590px");
             $("#PromptMessage").html("请先将累计未完成的补回情况填写完后，再填写当月的补回情况，未完成原因及措施。");
             $("#PromptMessage").addClass("Tishi");
@@ -57,12 +53,10 @@ function setArrow_nLeft(sender) {
             return true;
             break;
 
-        
+
         case "monthReportReady":
-            $(".tip_box").css("display", "block");
-            $(".tip").css("display", "block");
             if (confirm("请您确认是否保存月度上报")) {
-                $(".arrow_n").css("left", "760px");
+                $(".arrow_n").css("left", "930px");
                 $("#PromptMessage").html("请保存数据，等待其它上报人员填报。");
                 $("#PromptMessage").removeClass("Tishi");
                 return true;
@@ -71,9 +65,7 @@ function setArrow_nLeft(sender) {
             }
             break;
         case "monthReport":
-            $(".tip_box").css("display", "block");
-            $(".tip").css("display", "block");
-            $(".arrow_n").css("left", "930px");
+            $(".arrow_n").css("left", "760px");
             $("#PromptMessage").html("请填写月报说明。");
             $("#PromptMessage").removeClass("Tishi");
             $("#MonthReportLog").css("display", "block");
@@ -94,7 +86,7 @@ function setArrow_nLeft(sender) {
                 return false;
             }
             break;
-       
+
 
     }
 }
@@ -114,9 +106,9 @@ function setStlye(sender) {
 $(function setTitle() {
     var j = 0;
     $(".postion_num").each(function (i, item) {
-        
+
         if ("undefined" != typeof ItemDisplay) {
-            if (ItemDisplay + "Title" != item.id && ItemDisplay1 + "Title" != item.id  ) {
+            if (ItemDisplay + "Title" != item.id && ItemDisplay1 + "Title" != item.id) {
                 item.innerHTML = j * 1 + 1;
                 j++;
             } else {
