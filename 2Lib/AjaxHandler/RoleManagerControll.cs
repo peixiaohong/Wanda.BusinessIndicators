c2398af0d55cbd8e6b3bcaf6d23c8b1fbd99cad5
zhaotou94 <719007475@qq.com>
2018-04-16 20:01:14 +0800

    角色设置用户信息保存方法修订

--- 2Lib/AjaxHandler/RoleManagerControll.cs
+++ 2Lib/AjaxHandler/RoleManagerControll.cs
@@ -356,12 +356,12 @@ namespace LJTH.BusinessIndicators.Web.AjaxHandler
         /// <param name="data"></param>
         /// <returns></returns>
         [LibAction]
-        public object SaveUser_Role(List<string> LoginNames, string RoleID)
+        public object SaveUser_Role(string PageLoginNames, string RoleID)
         {
             string Message = string.Empty;
             int Success = 0;
 
-            if (RoleID == "")
+            if (RoleID == "" || PageLoginNames == "")
             {
                 return new
                 {
@@ -375,6 +375,16 @@ namespace LJTH.BusinessIndicators.Web.AjaxHandler
             {
                 List<string> oldLoginNames = S_Role_UserActionOperator.Instance.GetDataByRoleID(RoleID.ToGuid());
                 List<S_Role_User> entitys = new List<S_Role_User>();
+                string[] LoginNames = PageLoginNames.Split(',');
+                if (LoginNames.Count() < 1)
+                {
+                    return new
+                    {
+                        Data = "",
+                        Success = 0,
+                        Message = "参数丢失"
+                    };
+                }
                 foreach (var item in LoginNames)
                 {
                     if (oldLoginNames.Count > 0)
