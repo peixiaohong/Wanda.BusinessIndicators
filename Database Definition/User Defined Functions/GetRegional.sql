c3e587d794b9bd5ebf66487e7344a309b42c2771
zhaotou94 <719007475@qq.com>
2018-04-26 18:20:03 +0800

    新增自定义函数：得到项目的父级项目下项目授权的人

---
+++ Database Definition/User Defined Functions/GetRegional.sql
@@ -0,0 +1,15 @@
+﻿Create Function GetRegional(@OrgParentID UniqueIdentifier)
+returns @LoginNames Table
+(
+	LoginName nvarchar(Max)
+)
+As
+Begin
+
+	Declare  @SubID UniqueIdentifier;
+	Set @SubID=(Select Top 1 ID From [dbo].[S_Organizational] Where [ParentID]=@OrgParentID And [IsDeleted]=0)
+
+	 Insert Into @LoginNames
+	 Select [LoginName] From [dbo].[S_Org_User] Where [CompanyID]=@SubID
+	 Return 
+End 
