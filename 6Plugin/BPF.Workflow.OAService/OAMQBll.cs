ea1395b3e4ca33415d837aa95e697e5388f7db9f
Lei Yu <leyu1@microsoft.com>
2018-05-15 09:34:31 +0800

    填报人节点appURL为空

--- 6Plugin/BPF.Workflow.OAService/OAMQBll.cs
+++ 6Plugin/BPF.Workflow.OAService/OAMQBll.cs
@@ -229,7 +229,14 @@ namespace BPF.OAMQServices
             paramList.Add("workflowname", message.Flowtype);
             paramList.Add("nodename", message.Nodename);
             paramList.Add("pcurl", SSOToolkit.Instance.GetAuthOAUrlWithSSO(message.PtpUrl.Trim(), message.Userid, message.FlowID));
-            paramList.Add("appurl", SSOToolkit.Instance.GetAuthOAUrlWithSSO(message.MobileUrl.Trim(), message.Userid, message.FlowID));
+            if (message.Nodename=="填报人")
+            {
+                paramList.Add("appurl", "");
+            }
+            else
+            {
+                paramList.Add("appurl", SSOToolkit.Instance.GetAuthOAUrlWithSSO(message.MobileUrl.Trim(), message.Userid, message.FlowID));
+            }
             paramList.Add("creator", message.Creator);
             paramList.Add("isremark", message.Flowmess.ToString());
             //paramList.Add("viewtype", message.Viewtype.ToString());
