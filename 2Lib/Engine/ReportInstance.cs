12c64ee0ec6439e9ec374593ac4eac4527724475
zyg07 <zrg525700@163.com>
2018-05-16 13:47:34 +0800

    上月累计的计划值计算错误问题

--- 2Lib/Engine/ReportInstance.cs
+++ 2Lib/Engine/ReportInstance.cs
@@ -69,6 +69,7 @@ namespace LJTH.BusinessIndicators.Engine
             FinYear = report.FinYear;
             FinMonth = report.FinMonth;
             DataSource = _DataSource;
+            TargetPlanID = report.TargetPlanID;
             InitialData(IsLatestVersion, IsAll);
 
         }
