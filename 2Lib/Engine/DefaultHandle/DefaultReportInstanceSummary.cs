1a2e410819849e9fdfc3299af787b12b5f5877f5
zyg07 <zrg525700@163.com>
2018-06-14 18:17:03 +0800

    修改全年累计计划值

--- 2Lib/Engine/DefaultHandle/DefaultReportInstanceSummary.cs
+++ 2Lib/Engine/DefaultHandle/DefaultReportInstanceSummary.cs
@@ -154,7 +154,8 @@ namespace LJTH.BusinessIndicators.Engine
                             }
                             else
                             {
-                                mrsvm.MeasureRate = Math.Round(StaticResource.Instance.GetTargetPlanList(_System.ID, FinYear, MRDList[0].TargetPlanID).FindAll(P => P.TargetID == target.ID).Sum(P => P.Target), 7, MidpointRounding.AwayFromZero).ToString();
+                                var companyIdList = MRDList.Select(v => v.CompanyID).Distinct().ToList();
+                                mrsvm.MeasureRate = Math.Round(StaticResource.Instance.GetTargetPlanList(_System.ID, FinYear, MRDList[0].TargetPlanID).FindAll(P => P.TargetID == target.ID && companyIdList.Contains(P.CompanyID)).Sum(P => P.Target), 7, MidpointRounding.AwayFromZero).ToString();
                             }
 
 
